e3f8e0a35d8b46ebf933e62161e18f9e403cc9e0
ProsESportu <t.hajduga@poczta.fm>
2023-07-11 14:45:16 +0200

    google

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,17 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import GitHub from "@auth/core/providers/github";
-import { AUTH_SECRET, GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
+import Google from "@auth/core/providers/google";
+import { AUTH_SECRET, GITHUB_ID, GITHUB_SECRET, GOOGLE_CLIENT_ID, GOOGLE_SECRET } from "$env/static/private";
 import { prisma } from "$lib/prisma.server";
 import type { SessionWithId } from "$lib/types";
 
 export const handle = SvelteKitAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+    Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_SECRET })
+  ],
   secret: AUTH_SECRET,
   callbacks: {
     session: (e): SessionWithId => {
